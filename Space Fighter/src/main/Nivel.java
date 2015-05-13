c4dbbffad3ad8592bd7658683dc65d0bcad7c4ce
TechTalentsTechPro <jorgehuertatoledo@gmail.com>
2015-05-13 18:41:33 +0200

    Space Fighter: GUI Fixes Continued

--- Space Fighter/src/main/Nivel.java
+++ Space Fighter/src/main/Nivel.java
@@ -6,13 +6,17 @@ import es.techtalents.ttgdl.gui.window.Window;
 import es.techtalents.ttgdl.image.ImageLoader;
 
 public class Nivel extends Window{
-	public int dificulty; 
-	public Nivel(Game game) {
+	public int dificulty;
+	
+	public Nivel(Game game,int i) {
 		Image fondo = ImageLoader.loadImage("Images/fondoLevel.jpg").getScaledInstance(Game.WIDTH, Game.HEIGHT, Image.SCALE_SMOOTH);
 		setHeight(Game.HEIGHT);
 		setWidth(Game.WIDTH);
 		setBackgroundImage(fondo);
-		// TODO Auto-generated constructor stub
+		dificulty = i;
+	}
+	public void setDificulty(int i) {
+		dificulty = i;
 	}
 
 }
